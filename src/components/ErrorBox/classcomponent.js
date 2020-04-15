657fd39f7d9efe86fcee22e0b6dd90fdd51e00d1
Daniel Sousa <mail@danielsousa.io>
2020-04-15 22:27:25 +0100

    Update CalendarLogin JSX

--- src/components/ErrorBox/classcomponent.js
+++ src/components/ErrorBox/classcomponent.js
@@ -48,25 +48,25 @@ class CalendarLogin extends Component {
     }
 
     return (
-      <CbFlex
-        title="Button123"
-        color="#FF8040"
-        styles={{ overflow: "visible" }}
-      >
-        <CbFlex styles={{ overflow: "visible" }} />
-      </CbFlex>
+      <Layout style={styles.signInContainer}>
+        <Text style={styles.textRow}>SignIn with your Google account</Text>
+        <GoogleSigninButton
+          style={styles.loginButton}
+          color={GoogleSigninButton.Color.Light}
+          size={GoogleSigninButton.Size.Wide}
+          onPress={actions.googleSignIn}
+          disabled={this.state.isSigninInProgress}
+        />
+      </Layout>
     )
   }
 
   render() {
     return (
-      <CbFlex
-        title="Button123"
-        color="#FF8040"
-        styles={{ overflow: "visible" }}
-      >
-        <CbFlex styles={{ overflow: "visible" }} />
-      </CbFlex>
+      <Layout style={styles.screen}>
+        {this.renderUser()}
+        {this.renderErrors()}
+      </Layout>
     )
   }
 }
