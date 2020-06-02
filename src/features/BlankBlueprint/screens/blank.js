cfad62384781f2dc4818b0627328f0b25faa7b3c
Crowdbotics <git@crowdbotics.com>
2020-06-02 14:41:00 +0000

    Update components

--- src/features/BlankBlueprint/screens/blank.js
+++ src/features/BlankBlueprint/screens/blank.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { View } from "react-native";
-import {
-  withStyles,
-} from 'react-native-ui-kitten';
+import React, { Component } from "react"
+import { View } from "react-native"
+import { Text, Layout, Button } from "react-native-ui-kitten"
+import { connect } from "react-redux"
+import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
+import { styles } from "./styles"
+import * as calendarActions from "../../redux/actions"
+import ErrorBox from "../../../../components/ErrorBox"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <CbFlex title="Button12345" color="#FF8040" style={{ overflow: "visible" }}>
+      <CbFlex style={{ overflow: "visible" }} />
+    </CbFlex>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
