2fb0614745ae8db7f91f5004c3512ed7bfb13d03
Daniel Sousa <mail@danielsousa.io>
2020-07-03 17:18:12 +0100

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,6 +4,66 @@ const initialState = {}
 
 export default function apiReducer(state = initialState, action) {
   switch (action.type) {
+    case types.UPDATE_PET:
+    case types.UPDATE_PET_SUCCEEDED:
+    case types.UPDATE_PET_FAILED:
+    case types.ADD_PET:
+    case types.ADD_PET_SUCCEEDED:
+    case types.ADD_PET_FAILED:
+    case types.FIND_PETS_BY_STATUS:
+    case types.FIND_PETS_BY_STATUS_SUCCEEDED:
+    case types.FIND_PETS_BY_STATUS_FAILED:
+    case types.FIND_PETS_BY_TAGS:
+    case types.FIND_PETS_BY_TAGS_SUCCEEDED:
+    case types.FIND_PETS_BY_TAGS_FAILED:
+    case types.GET_PET_BY_ID:
+    case types.GET_PET_BY_ID_SUCCEEDED:
+    case types.GET_PET_BY_ID_FAILED:
+    case types.UPDATE_PET_WITH_FORM:
+    case types.UPDATE_PET_WITH_FORM_SUCCEEDED:
+    case types.UPDATE_PET_WITH_FORM_FAILED:
+    case types.DELETE_PET:
+    case types.DELETE_PET_SUCCEEDED:
+    case types.DELETE_PET_FAILED:
+    case types.UPLOAD_FILE:
+    case types.UPLOAD_FILE_SUCCEEDED:
+    case types.UPLOAD_FILE_FAILED:
+    case types.GET_INVENTORY:
+    case types.GET_INVENTORY_SUCCEEDED:
+    case types.GET_INVENTORY_FAILED:
+    case types.PLACE_ORDER:
+    case types.PLACE_ORDER_SUCCEEDED:
+    case types.PLACE_ORDER_FAILED:
+    case types.GET_ORDER_BY_ID:
+    case types.GET_ORDER_BY_ID_SUCCEEDED:
+    case types.GET_ORDER_BY_ID_FAILED:
+    case types.DELETE_ORDER:
+    case types.DELETE_ORDER_SUCCEEDED:
+    case types.DELETE_ORDER_FAILED:
+    case types.CREATE_USER:
+    case types.CREATE_USER_SUCCEEDED:
+    case types.CREATE_USER_FAILED:
+    case types.CREATE_USERS_WITH_ARRAY_INPUT:
+    case types.CREATE_USERS_WITH_ARRAY_INPUT_SUCCEEDED:
+    case types.CREATE_USERS_WITH_ARRAY_INPUT_FAILED:
+    case types.CREATE_USERS_WITH_LIST_INPUT:
+    case types.CREATE_USERS_WITH_LIST_INPUT_SUCCEEDED:
+    case types.CREATE_USERS_WITH_LIST_INPUT_FAILED:
+    case types.LOGIN_USER:
+    case types.LOGIN_USER_SUCCEEDED:
+    case types.LOGIN_USER_FAILED:
+    case types.LOGOUT_USER:
+    case types.LOGOUT_USER_SUCCEEDED:
+    case types.LOGOUT_USER_FAILED:
+    case types.GET_USER_BY_NAME:
+    case types.GET_USER_BY_NAME_SUCCEEDED:
+    case types.GET_USER_BY_NAME_FAILED:
+    case types.UPDATE_USER:
+    case types.UPDATE_USER_SUCCEEDED:
+    case types.UPDATE_USER_FAILED:
+    case types.DELETE_USER:
+    case types.DELETE_USER_SUCCEEDED:
+    case types.DELETE_USER_FAILED:
     default:
       return state
   }
--- src/store/services.js
+++ src/store/services.js
@@ -1,2 +1,95 @@
 import axios from "axios"
-export const apiService = {}
+const swaggerPetstore = axios.create({
+  baseURL: "https://petstore.swagger.io/v2",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    api_key: process.env.SERVICE_API_KEY
+  }
+})
+function updatePet(requestBody) {
+  return swaggerPetstore.put(`/pet`, requestBody)
+}
+function addPet(requestBody) {
+  return swaggerPetstore.post(`/pet`, requestBody)
+}
+function findPetsByStatus({ status }) {
+  return swaggerPetstore.get(`/pet/findByStatus`, {
+    params: { status: status }
+  })
+}
+function findPetsByTags({ tags }) {
+  return swaggerPetstore.get(`/pet/findByTags`, { params: { tags: tags } })
+}
+function getPetById(petId) {
+  return swaggerPetstore.get(`/pet/{petId}`)
+}
+function updatePetWithForm(petId, requestBody) {
+  return swaggerPetstore.post(`/pet/{petId}`, requestBody)
+}
+function deletePet(petId) {
+  return swaggerPetstore.delete(`/pet/{petId}`)
+}
+function uploadFile(petId, requestBody) {
+  return swaggerPetstore.post(`/pet/{petId}/uploadImage`, requestBody)
+}
+function getInventory() {
+  return swaggerPetstore.get(`/store/inventory`)
+}
+function placeOrder(requestBody) {
+  return swaggerPetstore.post(`/store/order`, requestBody)
+}
+function getOrderById(orderId) {
+  return swaggerPetstore.get(`/store/order/{orderId}`)
+}
+function deleteOrder(orderId) {
+  return swaggerPetstore.delete(`/store/order/{orderId}`)
+}
+function createUser(requestBody) {
+  return swaggerPetstore.post(`/user`, requestBody)
+}
+function createUsersWithArrayInput(requestBody) {
+  return swaggerPetstore.post(`/user/createWithArray`, requestBody)
+}
+function createUsersWithListInput(requestBody) {
+  return swaggerPetstore.post(`/user/createWithList`, requestBody)
+}
+function loginUser({ username, password }) {
+  return swaggerPetstore.get(`/user/login`, {
+    params: { username: username, password: password }
+  })
+}
+function logoutUser() {
+  return swaggerPetstore.get(`/user/logout`)
+}
+function getUserByName(username) {
+  return swaggerPetstore.get(`/user/{username}`)
+}
+function updateUser(username, requestBody) {
+  return swaggerPetstore.put(`/user/{username}`, requestBody)
+}
+function deleteUser(username) {
+  return swaggerPetstore.delete(`/user/{username}`)
+}
+export const apiService = {
+  updatePet,
+  addPet,
+  findPetsByStatus,
+  findPetsByTags,
+  getPetById,
+  updatePetWithForm,
+  deletePet,
+  uploadFile,
+  getInventory,
+  placeOrder,
+  getOrderById,
+  deleteOrder,
+  createUser,
+  createUsersWithArrayInput,
+  createUsersWithListInput,
+  loginUser,
+  logoutUser,
+  getUserByName,
+  updateUser,
+  deleteUser
+}
