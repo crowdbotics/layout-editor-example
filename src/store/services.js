89e558b3fdd1448de19e5eb77429b489f068f5fd
Crowdbotics <git@crowdbotics.com>
2020-07-03 16:38:17 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -6,6 +6,14 @@ const swaggerPetstore = axios.create({
     api_key: process.env.SERVICE_API_KEY
   }
 })
+const testePermissionsAPI = axios.create({
+  baseURL: "https://testepermissions_15043.herokuapp.com/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    api_key: process.env.SERVICE_API_KEY
+  }
+})
 function updatePet(requestBody) {
   return swaggerPetstore.put(`/pet`, requestBody)
 }
@@ -70,6 +78,75 @@ function updateUser(username, requestBody) {
 function deleteUser(username) {
   return swaggerPetstore.delete(`/user/{username}`)
 }
+function api_v1_customtext_list() {
+  return testePermissionsAPI.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read() {
+  return testePermissionsAPI.get(`/api/v1/customtext/{id}/`)
+}
+function api_v1_customtext_update(requestBody) {
+  return testePermissionsAPI.put(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_customtext_partial_update(requestBody) {
+  return testePermissionsAPI.patch(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_homepage_list() {
+  return testePermissionsAPI.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read() {
+  return testePermissionsAPI.get(`/api/v1/homepage/{id}/`)
+}
+function api_v1_homepage_update(requestBody) {
+  return testePermissionsAPI.put(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_homepage_partial_update(requestBody) {
+  return testePermissionsAPI.patch(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_login_create() {
+  return testePermissionsAPI.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(requestBody) {
+  return testePermissionsAPI.post(`/api/v1/signup/`, requestBody)
+}
+function rest_auth_login_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/login/`, requestBody)
+}
+function rest_auth_logout_list() {
+  return testePermissionsAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create() {
+  return testePermissionsAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/password/change/`, requestBody)
+}
+function rest_auth_password_reset_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/password/reset/`, requestBody)
+}
+function rest_auth_password_reset_confirm_create(requestBody) {
+  return testePermissionsAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    requestBody
+  )
+}
+function rest_auth_registration_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/registration/`, requestBody)
+}
+function rest_auth_registration_verify_email_create(requestBody) {
+  return testePermissionsAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    requestBody
+  )
+}
+function rest_auth_user_read() {
+  return testePermissionsAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(requestBody) {
+  return testePermissionsAPI.put(`/rest-auth/user/`, requestBody)
+}
+function rest_auth_user_partial_update(requestBody) {
+  return testePermissionsAPI.patch(`/rest-auth/user/`, requestBody)
+}
 export const apiService = {
   updatePet,
   addPet,
@@ -90,5 +167,26 @@ export const apiService = {
   logoutUser,
   getUserByName,
   updateUser,
-  deleteUser
+  deleteUser,
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
 }
