ed7df4f20abbfe702f2d24d9bc7d1e864904eacb
Daniel Sousa <mail@danielsousa.io>
2020-07-06 20:03:52 +0100

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -246,3 +246,224 @@ export const deleteUserFailed = error => ({
   type: types.DELETE_USER_FAILED,
   error
 })
+export const api_v1_customtext_list = () => ({
+  type: types.API_V1_CUSTOMTEXT_LIST
+})
+export const api_v1_customtext_listSucceeded = response => ({
+  type: types.API_V1_CUSTOMTEXT_LIST_SUCCEEDED,
+  response
+})
+export const api_v1_customtext_listFailed = error => ({
+  type: types.API_V1_CUSTOMTEXT_LIST_FAILED,
+  error
+})
+export const api_v1_customtext_read = () => ({
+  type: types.API_V1_CUSTOMTEXT_READ
+})
+export const api_v1_customtext_readSucceeded = response => ({
+  type: types.API_V1_CUSTOMTEXT_READ_SUCCEEDED,
+  response
+})
+export const api_v1_customtext_readFailed = error => ({
+  type: types.API_V1_CUSTOMTEXT_READ_FAILED,
+  error
+})
+export const api_v1_customtext_update = () => ({
+  type: types.API_V1_CUSTOMTEXT_UPDATE
+})
+export const api_v1_customtext_updateSucceeded = response => ({
+  type: types.API_V1_CUSTOMTEXT_UPDATE_SUCCEEDED,
+  response
+})
+export const api_v1_customtext_updateFailed = error => ({
+  type: types.API_V1_CUSTOMTEXT_UPDATE_FAILED,
+  error
+})
+export const api_v1_customtext_partial_update = () => ({
+  type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE
+})
+export const api_v1_customtext_partial_updateSucceeded = response => ({
+  type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_SUCCEEDED,
+  response
+})
+export const api_v1_customtext_partial_updateFailed = error => ({
+  type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_FAILED,
+  error
+})
+export const api_v1_homepage_list = () => ({ type: types.API_V1_HOMEPAGE_LIST })
+export const api_v1_homepage_listSucceeded = response => ({
+  type: types.API_V1_HOMEPAGE_LIST_SUCCEEDED,
+  response
+})
+export const api_v1_homepage_listFailed = error => ({
+  type: types.API_V1_HOMEPAGE_LIST_FAILED,
+  error
+})
+export const api_v1_homepage_read = () => ({ type: types.API_V1_HOMEPAGE_READ })
+export const api_v1_homepage_readSucceeded = response => ({
+  type: types.API_V1_HOMEPAGE_READ_SUCCEEDED,
+  response
+})
+export const api_v1_homepage_readFailed = error => ({
+  type: types.API_V1_HOMEPAGE_READ_FAILED,
+  error
+})
+export const api_v1_homepage_update = () => ({
+  type: types.API_V1_HOMEPAGE_UPDATE
+})
+export const api_v1_homepage_updateSucceeded = response => ({
+  type: types.API_V1_HOMEPAGE_UPDATE_SUCCEEDED,
+  response
+})
+export const api_v1_homepage_updateFailed = error => ({
+  type: types.API_V1_HOMEPAGE_UPDATE_FAILED,
+  error
+})
+export const api_v1_homepage_partial_update = () => ({
+  type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE
+})
+export const api_v1_homepage_partial_updateSucceeded = response => ({
+  type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE_SUCCEEDED,
+  response
+})
+export const api_v1_homepage_partial_updateFailed = error => ({
+  type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE_FAILED,
+  error
+})
+export const api_v1_login_create = () => ({ type: types.API_V1_LOGIN_CREATE })
+export const api_v1_login_createSucceeded = response => ({
+  type: types.API_V1_LOGIN_CREATE_SUCCEEDED,
+  response
+})
+export const api_v1_login_createFailed = error => ({
+  type: types.API_V1_LOGIN_CREATE_FAILED,
+  error
+})
+export const api_v1_signup_create = () => ({ type: types.API_V1_SIGNUP_CREATE })
+export const api_v1_signup_createSucceeded = response => ({
+  type: types.API_V1_SIGNUP_CREATE_SUCCEEDED,
+  response
+})
+export const api_v1_signup_createFailed = error => ({
+  type: types.API_V1_SIGNUP_CREATE_FAILED,
+  error
+})
+export const rest_auth_login_create = () => ({
+  type: types.REST_AUTH_LOGIN_CREATE
+})
+export const rest_auth_login_createSucceeded = response => ({
+  type: types.REST_AUTH_LOGIN_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_login_createFailed = error => ({
+  type: types.REST_AUTH_LOGIN_CREATE_FAILED,
+  error
+})
+export const rest_auth_logout_list = () => ({
+  type: types.REST_AUTH_LOGOUT_LIST
+})
+export const rest_auth_logout_listSucceeded = response => ({
+  type: types.REST_AUTH_LOGOUT_LIST_SUCCEEDED,
+  response
+})
+export const rest_auth_logout_listFailed = error => ({
+  type: types.REST_AUTH_LOGOUT_LIST_FAILED,
+  error
+})
+export const rest_auth_logout_create = () => ({
+  type: types.REST_AUTH_LOGOUT_CREATE
+})
+export const rest_auth_logout_createSucceeded = response => ({
+  type: types.REST_AUTH_LOGOUT_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_logout_createFailed = error => ({
+  type: types.REST_AUTH_LOGOUT_CREATE_FAILED,
+  error
+})
+export const rest_auth_password_change_create = () => ({
+  type: types.REST_AUTH_PASSWORD_CHANGE_CREATE
+})
+export const rest_auth_password_change_createSucceeded = response => ({
+  type: types.REST_AUTH_PASSWORD_CHANGE_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_password_change_createFailed = error => ({
+  type: types.REST_AUTH_PASSWORD_CHANGE_CREATE_FAILED,
+  error
+})
+export const rest_auth_password_reset_create = () => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CREATE
+})
+export const rest_auth_password_reset_createSucceeded = response => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_password_reset_createFailed = error => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CREATE_FAILED,
+  error
+})
+export const rest_auth_password_reset_confirm_create = () => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE
+})
+export const rest_auth_password_reset_confirm_createSucceeded = response => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_password_reset_confirm_createFailed = error => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_FAILED,
+  error
+})
+export const rest_auth_registration_create = () => ({
+  type: types.REST_AUTH_REGISTRATION_CREATE
+})
+export const rest_auth_registration_createSucceeded = response => ({
+  type: types.REST_AUTH_REGISTRATION_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_registration_createFailed = error => ({
+  type: types.REST_AUTH_REGISTRATION_CREATE_FAILED,
+  error
+})
+export const rest_auth_registration_verify_email_create = () => ({
+  type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE
+})
+export const rest_auth_registration_verify_email_createSucceeded = response => ({
+  type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_SUCCEEDED,
+  response
+})
+export const rest_auth_registration_verify_email_createFailed = error => ({
+  type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_FAILED,
+  error
+})
+export const rest_auth_user_read = () => ({ type: types.REST_AUTH_USER_READ })
+export const rest_auth_user_readSucceeded = response => ({
+  type: types.REST_AUTH_USER_READ_SUCCEEDED,
+  response
+})
+export const rest_auth_user_readFailed = error => ({
+  type: types.REST_AUTH_USER_READ_FAILED,
+  error
+})
+export const rest_auth_user_update = () => ({
+  type: types.REST_AUTH_USER_UPDATE
+})
+export const rest_auth_user_updateSucceeded = response => ({
+  type: types.REST_AUTH_USER_UPDATE_SUCCEEDED,
+  response
+})
+export const rest_auth_user_updateFailed = error => ({
+  type: types.REST_AUTH_USER_UPDATE_FAILED,
+  error
+})
+export const rest_auth_user_partial_update = () => ({
+  type: types.REST_AUTH_USER_PARTIAL_UPDATE
+})
+export const rest_auth_user_partial_updateSucceeded = response => ({
+  type: types.REST_AUTH_USER_PARTIAL_UPDATE_SUCCEEDED,
+  response
+})
+export const rest_auth_user_partial_updateFailed = error => ({
+  type: types.REST_AUTH_USER_PARTIAL_UPDATE_FAILED,
+  error
+})
--- src/store/services.js
+++ src/store/services.js
@@ -4,75 +4,154 @@ import {
   TESTEPERMISSIONS_API_USERNAME,
   TESTEPERMISSIONS_API_PASSWORD
 } from "react-native-dotenv"
-const apiClient = axios.create({
+const swaggerPetstore = axios.create({
   baseURL: "https://petstore.swagger.io/v2",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
-    api_key: process.env.SERVICE_API_KEY
+    api_key: PET_STORE_API_KEY
   }
 })
+const testePermissionsAPI = axios.create({
+  baseURL: "https://testepermissions_15043.herokuapp.com/",
+  auth: {
+    username: TESTEPERMISSIONS_API_USERNAME,
+    password: TESTEPERMISSIONS_API_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function updatePet(requestBody) {
-  return apiClient.put(`/pet`, requestBody)
+  return swaggerPetstore.put(`/pet`, requestBody)
 }
 function addPet(requestBody) {
-  return apiClient.post(`/pet`, requestBody)
+  return swaggerPetstore.post(`/pet`, requestBody)
 }
 function findPetsByStatus({ status }) {
-  return apiClient.get(`/pet/findByStatus`, { params: { status: status } })
+  return swaggerPetstore.get(`/pet/findByStatus`, {
+    params: { status: status }
+  })
 }
 function findPetsByTags({ tags }) {
-  return apiClient.get(`/pet/findByTags`, { params: { tags: tags } })
+  return swaggerPetstore.get(`/pet/findByTags`, { params: { tags: tags } })
 }
 function getPetById(petId) {
-  return apiClient.get(`/pet/{petId}`)
+  return swaggerPetstore.get(`/pet/{petId}`)
 }
 function updatePetWithForm(petId, requestBody) {
-  return apiClient.post(`/pet/{petId}`, requestBody)
+  return swaggerPetstore.post(`/pet/{petId}`, requestBody)
 }
 function deletePet(petId) {
-  return apiClient.delete(`/pet/{petId}`)
+  return swaggerPetstore.delete(`/pet/{petId}`)
 }
 function uploadFile(petId, requestBody) {
-  return apiClient.post(`/pet/{petId}/uploadImage`, requestBody)
+  return swaggerPetstore.post(`/pet/{petId}/uploadImage`, requestBody)
 }
 function getInventory() {
-  return apiClient.get(`/store/inventory`)
+  return swaggerPetstore.get(`/store/inventory`)
 }
 function placeOrder(requestBody) {
-  return apiClient.post(`/store/order`, requestBody)
+  return swaggerPetstore.post(`/store/order`, requestBody)
 }
 function getOrderById(orderId) {
-  return apiClient.get(`/store/order/{orderId}`)
+  return swaggerPetstore.get(`/store/order/{orderId}`)
 }
 function deleteOrder(orderId) {
-  return apiClient.delete(`/store/order/{orderId}`)
+  return swaggerPetstore.delete(`/store/order/{orderId}`)
 }
 function createUser(requestBody) {
-  return apiClient.post(`/user`, requestBody)
+  return swaggerPetstore.post(`/user`, requestBody)
 }
 function createUsersWithArrayInput(requestBody) {
-  return apiClient.post(`/user/createWithArray`, requestBody)
+  return swaggerPetstore.post(`/user/createWithArray`, requestBody)
 }
 function createUsersWithListInput(requestBody) {
-  return apiClient.post(`/user/createWithList`, requestBody)
+  return swaggerPetstore.post(`/user/createWithList`, requestBody)
 }
 function loginUser({ username, password }) {
-  return apiClient.get(`/user/login`, {
+  return swaggerPetstore.get(`/user/login`, {
     params: { username: username, password: password }
   })
 }
 function logoutUser() {
-  return apiClient.get(`/user/logout`)
+  return swaggerPetstore.get(`/user/logout`)
 }
 function getUserByName(username) {
-  return apiClient.get(`/user/{username}`)
+  return swaggerPetstore.get(`/user/{username}`)
 }
 function updateUser(username, requestBody) {
-  return apiClient.put(`/user/{username}`, requestBody)
+  return swaggerPetstore.put(`/user/{username}`, requestBody)
 }
 function deleteUser(username) {
-  return apiClient.delete(`/user/{username}`)
+  return swaggerPetstore.delete(`/user/{username}`)
+}
+function api_v1_customtext_list() {
+  return testePermissionsAPI.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read() {
+  return testePermissionsAPI.get(`/api/v1/customtext/{id}/`)
+}
+function api_v1_customtext_update(requestBody) {
+  return testePermissionsAPI.put(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_customtext_partial_update(requestBody) {
+  return testePermissionsAPI.patch(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_homepage_list() {
+  return testePermissionsAPI.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read() {
+  return testePermissionsAPI.get(`/api/v1/homepage/{id}/`)
+}
+function api_v1_homepage_update(requestBody) {
+  return testePermissionsAPI.put(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_homepage_partial_update(requestBody) {
+  return testePermissionsAPI.patch(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_login_create() {
+  return testePermissionsAPI.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(requestBody) {
+  return testePermissionsAPI.post(`/api/v1/signup/`, requestBody)
+}
+function rest_auth_login_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/login/`, requestBody)
+}
+function rest_auth_logout_list() {
+  return testePermissionsAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create() {
+  return testePermissionsAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/password/change/`, requestBody)
+}
+function rest_auth_password_reset_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/password/reset/`, requestBody)
+}
+function rest_auth_password_reset_confirm_create(requestBody) {
+  return testePermissionsAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    requestBody
+  )
+}
+function rest_auth_registration_create(requestBody) {
+  return testePermissionsAPI.post(`/rest-auth/registration/`, requestBody)
+}
+function rest_auth_registration_verify_email_create(requestBody) {
+  return testePermissionsAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    requestBody
+  )
+}
+function rest_auth_user_read() {
+  return testePermissionsAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(requestBody) {
+  return testePermissionsAPI.put(`/rest-auth/user/`, requestBody)
+}
+function rest_auth_user_partial_update(requestBody) {
+  return testePermissionsAPI.patch(`/rest-auth/user/`, requestBody)
 }
 export const apiService = {
   updatePet,
@@ -94,5 +173,26 @@ export const apiService = {
   logoutUser,
   getUserByName,
   updateUser,
-  deleteUser
+  deleteUser,
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
 }
