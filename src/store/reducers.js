cf3677fbf6929b046b68c51baea8fb89c0ecd970
Daniel Sousa <mail@danielsousa.io>
2020-07-06 15:25:18 +0100

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,8 +1,5 @@
 import * as types from "./constants"
-
-const initialState = {}
-
-export default function apiReducer(state = initialState, action) {
+export default function apiReducer(state = {}, action) {
   switch (action.type) {
     case types.UPDATE_PET:
     case types.UPDATE_PET_SUCCEEDED:
@@ -64,69 +61,6 @@ export default function apiReducer(state = initialState, action) {
     case types.DELETE_USER:
     case types.DELETE_USER_SUCCEEDED:
     case types.DELETE_USER_FAILED:
-    case types.API_V1_CUSTOMTEXT_LIST:
-    case types.API_V1_CUSTOMTEXT_LIST_SUCCEEDED:
-    case types.API_V1_CUSTOMTEXT_LIST_FAILED:
-    case types.API_V1_CUSTOMTEXT_READ:
-    case types.API_V1_CUSTOMTEXT_READ_SUCCEEDED:
-    case types.API_V1_CUSTOMTEXT_READ_FAILED:
-    case types.API_V1_CUSTOMTEXT_UPDATE:
-    case types.API_V1_CUSTOMTEXT_UPDATE_SUCCEEDED:
-    case types.API_V1_CUSTOMTEXT_UPDATE_FAILED:
-    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE:
-    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_SUCCEEDED:
-    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_FAILED:
-    case types.API_V1_HOMEPAGE_LIST:
-    case types.API_V1_HOMEPAGE_LIST_SUCCEEDED:
-    case types.API_V1_HOMEPAGE_LIST_FAILED:
-    case types.API_V1_HOMEPAGE_READ:
-    case types.API_V1_HOMEPAGE_READ_SUCCEEDED:
-    case types.API_V1_HOMEPAGE_READ_FAILED:
-    case types.API_V1_HOMEPAGE_UPDATE:
-    case types.API_V1_HOMEPAGE_UPDATE_SUCCEEDED:
-    case types.API_V1_HOMEPAGE_UPDATE_FAILED:
-    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE:
-    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE_SUCCEEDED:
-    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE_FAILED:
-    case types.API_V1_LOGIN_CREATE:
-    case types.API_V1_LOGIN_CREATE_SUCCEEDED:
-    case types.API_V1_LOGIN_CREATE_FAILED:
-    case types.API_V1_SIGNUP_CREATE:
-    case types.API_V1_SIGNUP_CREATE_SUCCEEDED:
-    case types.API_V1_SIGNUP_CREATE_FAILED:
-    case types.REST_AUTH_LOGIN_CREATE:
-    case types.REST_AUTH_LOGIN_CREATE_SUCCEEDED:
-    case types.REST_AUTH_LOGIN_CREATE_FAILED:
-    case types.REST_AUTH_LOGOUT_LIST:
-    case types.REST_AUTH_LOGOUT_LIST_SUCCEEDED:
-    case types.REST_AUTH_LOGOUT_LIST_FAILED:
-    case types.REST_AUTH_LOGOUT_CREATE:
-    case types.REST_AUTH_LOGOUT_CREATE_SUCCEEDED:
-    case types.REST_AUTH_LOGOUT_CREATE_FAILED:
-    case types.REST_AUTH_PASSWORD_CHANGE_CREATE:
-    case types.REST_AUTH_PASSWORD_CHANGE_CREATE_SUCCEEDED:
-    case types.REST_AUTH_PASSWORD_CHANGE_CREATE_FAILED:
-    case types.REST_AUTH_PASSWORD_RESET_CREATE:
-    case types.REST_AUTH_PASSWORD_RESET_CREATE_SUCCEEDED:
-    case types.REST_AUTH_PASSWORD_RESET_CREATE_FAILED:
-    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE:
-    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_SUCCEEDED:
-    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_FAILED:
-    case types.REST_AUTH_REGISTRATION_CREATE:
-    case types.REST_AUTH_REGISTRATION_CREATE_SUCCEEDED:
-    case types.REST_AUTH_REGISTRATION_CREATE_FAILED:
-    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE:
-    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_SUCCEEDED:
-    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_FAILED:
-    case types.REST_AUTH_USER_READ:
-    case types.REST_AUTH_USER_READ_SUCCEEDED:
-    case types.REST_AUTH_USER_READ_FAILED:
-    case types.REST_AUTH_USER_UPDATE:
-    case types.REST_AUTH_USER_UPDATE_SUCCEEDED:
-    case types.REST_AUTH_USER_UPDATE_FAILED:
-    case types.REST_AUTH_USER_PARTIAL_UPDATE:
-    case types.REST_AUTH_USER_PARTIAL_UPDATE_SUCCEEDED:
-    case types.REST_AUTH_USER_PARTIAL_UPDATE_FAILED:
     default:
       return state
   }
