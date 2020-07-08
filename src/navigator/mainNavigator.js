bd143980c9d78f8ac8969755eb581d2b05ff2fac
Daniel <daniel.s@crowdbotics.com>
2020-07-08 14:41:19 +0100

    Add navigation

---
+++ src/navigator/mainNavigator.js
@@ -0,0 +1,36 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
+import SplashScreen from "../features/SplashScreen";
+import SideMenu from './sideMenu';
+//@BlueprintImportInsertion
+import BlankScreen48369Navigator from '../features/BlankScreen48369/navigator';
+
+/**
+ * new navigators can be imported here
+ */
+
+const AppNavigator = {
+    SplashScreen: {
+      screen: SplashScreen
+    },
+    //@BlueprintNavigationInsertion
+BlankScreen48369: { screen: BlankScreen48369Navigator },
+
+    /** new navigators can be added here */
+};
+
+const DrawerAppNavigator = createDrawerNavigator(
+  {
+    ...AppNavigator,
+  },
+  {
+    contentComponent: SideMenu,
+    initialRouteName: 'SplashScreen',
+  },
+);
+
+const AppContainer = createAppContainer(DrawerAppNavigator);
+
+export default AppContainer;
