4d53225ff5cc417b1564c3e0a1d0217e889eb62e
Crowdbotics <git@crowdbotics.com>
2020-07-23 04:48:27 +0000

    Update components

--- src/features/NewScreen/screens/index.js
+++ src/features/NewScreen/screens/index.js
@@ -1,25 +1,26 @@
-import React from "react"
+import React, { Component } from "react"
 import { View, Image, ImageBackground } from "react-native"
-import {
-  withStyles,
-  Text,
-  Button,
-  CheckBox,
-  Radio,
-  Toggle,
-  Icon,
-  Input,
-  Datepicker
-} from "react-native-ui-kitten"
+import { Text, Layout, Button } from "react-native-ui-kitten"
+
 import Slider from "@react-native-community/slider"
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
+import { styles } from "./styles"
+import * as calendarActions from "../../redux/actions"
+import ErrorBox from "../../../../components/ErrorBox"
 export class _New extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
-  render = () => <View style={this.props.themedStyle.container}></View>
+  render = () => (
+    <CbFlex title="Button12345" color="#FF8040" style={{ overflow: "visible" }}>
+      <CbFlex style={{ overflow: "visible" }} />
+    </CbFlex>
+  )
 }
+
 export default New = withStyles(_New, theme => ({
   container: { backgroundColor: theme["color-basic-100"] }
 }))
