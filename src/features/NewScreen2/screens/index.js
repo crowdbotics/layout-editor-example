654e7e163d981af47a9aed9cc9ab585b3cdeb8ac
Crowdbotics <git@crowdbotics.com>
2020-09-18 22:51:20 +0000

    Update components

--- src/features/NewScreen2/screens/index.js
+++ src/features/NewScreen2/screens/index.js
@@ -16,17 +16,15 @@ export class _New extends React.Component {
   }
   state = {}
   render = () => (
-    <CbFlex
-      title="Button12345"
-      color="#FF8040"
-      style={this.props.themedStyle.CbFlex_undefined}
-    >
-      <CbFlex style={this.props.themedStyle.CbFlex_undefined} />
+    <CbFlex title="Button12345" color="#FF8040" style={{ overflow: "visible" }}>
+      <CbFlex style={{ overflow: "visible" }} />
     </CbFlex>
   )
 }
 
-New = withStyles(_New, theme => ({}))
+New = withStyles(_New, theme => ({
+  container: { backgroundColor: theme["color-basic-100"] }
+}))
 
 function mapStateToProps(state) {
   return { state: state }
