41256fe3608ba9d1b591de91793105df6d6cd317
Daniel Sousa <mail@danielsousa.io>
2020-09-24 18:32:22 +0100

    Update components

--- src/features/BlankScreen4955/screens/blank.js
+++ src/features/BlankScreen4955/screens/blank.js
@@ -1,6 +1,16 @@
-import React, { Component } from "react"
+import React from "react"
 import { View } from "react-native"
-import { Text, Layout, Button } from "react-native-ui-kitten"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input
+} from "react-native-ui-kitten"
+
 import { connect } from "react-redux"
 import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
 import { styles } from "./styles"
@@ -13,14 +23,27 @@ export class _Blank extends React.Component {
   }
 
   render = () => (
-    <CbFlex
-      title="Button12345"
-      color="#FF8040"
-      style={this.props.themedStyle.CbFlex_undefined}
-    >
-      <CbFlex style={this.props.themedStyle.CbFlex_undefined} />
-    </CbFlex>
+    <View style={this.props.themedStyle.View_1}>
+      <Button
+        style={this.props.themedStyle.Button_4}
+        onPress={() => alert("Pressed!")}
+      >
+        Buttonoksomething
+      </Button>
+    </View>
   )
 }
 
-export default Blank = withStyles(_Blank, theme => ({}))
+export default Blank = withStyles(_Blank, theme => ({
+  View_1: {
+    overflow: "visible",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderStyle: "solid",
+    backgroundSize: "auto"
+  },
+  Button_4: { overflow: "visible", borderStyle: "solid" }
+}))
