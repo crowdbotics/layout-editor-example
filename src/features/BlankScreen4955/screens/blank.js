84649cb2c175a649f64af12e7c51ff65c87667c8
Crowdbotics <git@crowdbotics.com>
2020-10-01 19:09:55 +0000

    Update components

--- src/features/BlankScreen4955/screens/blank.js
+++ src/features/BlankScreen4955/screens/blank.js
@@ -1,16 +1,6 @@
-import React from "react"
+import React, { Component } from "react"
 import { View } from "react-native"
-import {
-  withStyles,
-  Text,
-  Button,
-  CheckBox,
-  Radio,
-  Toggle,
-  Icon,
-  Input
-} from "react-native-ui-kitten"
-
+import { Text, Layout, Button } from "react-native-ui-kitten"
 import { connect } from "react-redux"
 import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
 import { styles } from "./styles"
@@ -25,29 +15,14 @@ export class _Blank extends React.Component {
   state = {}
 
   render = () => (
-    <View style={styles.View_1}>
-      <Button
-        title="something"
-        style={styles.Button_4}
-        onPress={() => alert("Pressed!")}
-      >
-        Buttonok
-      </Button>
-    </View>
+    <CbFlex
+      title="Button12345"
+      color="#FF8040"
+      style={this.props.themedStyle.CbFlex_undefined}
+    >
+      <CbFlex style={this.props.themedStyle.CbFlex_undefined} />
+    </CbFlex>
   )
 }
 
-export default Blank = withStyles(_Blank, theme => ({
-  View_1: {
-    overflow: "visible",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderStyle: "solid",
-    backgroundSize: "auto"
-  },
-
-  Button_4: { overflow: "visible", borderStyle: "solid" }
-}))
+export default Blank = withStyles(_Blank, theme => ({}))
