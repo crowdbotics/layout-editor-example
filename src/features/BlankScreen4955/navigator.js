bed268d361046a9b30485f657e2cdc3dc8d04ab4
Daniel Sousa <mail@danielsousa.io>
2020-10-05 21:11:24 +0100

    Update components

--- src/features/BlankScreen4955/navigator.js
+++ src/features/BlankScreen4955/navigator.js
@@ -1,6 +1,6 @@
 import { createStackNavigator } from "react-navigation-stack"
-import Blank from "./screens/blank"
-export default BlankNavigator = createStackNavigator(
-  { Blank: { screen: Blank } },
-  { initialRouteName: "Blank" }
+import _Blank from "./screens/blank"
+export default _BlankNavigator = createStackNavigator(
+  { _Blank: { screen: _Blank } },
+  { initialRouteName: "_Blank" }
 )
