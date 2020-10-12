b7d9093dc6fb7447ddaa060d91a80423635f2d79
Daniel <daniel.s@crowdbotics.com>
2020-10-12 17:48:20 +0100

    Remove screens

--- src/features/BlankScreen4955/navigator.js
+++
@@ -1,6 +0,0 @@
-import { createStackNavigator } from "react-navigation-stack"
-import _Blank from "./screens/blank"
-export default _BlankNavigator = createStackNavigator(
-  { _Blank: { screen: _Blank } },
-  { initialRouteName: "_Blank" }
-)
