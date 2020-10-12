76ce51e54dce7edbf2be9a899d9c5e58ddd767d3
Daniel <daniel.s@crowdbotics.com>
2020-10-12 17:40:03 +0100

    Remove all screens

--- src/features/NewScreen/navigator.js
+++
@@ -1,6 +0,0 @@
-import { createStackNavigator } from "react-navigation-stack"
-import New from "./screens/index.js"
-export default NewNavigator = createStackNavigator(
-  { New: { screen: New } },
-  { initialRouteName: "New" }
-)
--- src/features/NewScreen3/navigator.js
+++
@@ -1,6 +0,0 @@
-import { createStackNavigator } from "react-navigation-stack"
-import New from "./screens/blank"
-export default NewNavigator = createStackNavigator(
-  { New: { screen: New } },
-  { initialRouteName: "New" }
-)
