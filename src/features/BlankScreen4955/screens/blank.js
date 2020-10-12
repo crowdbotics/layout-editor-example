62f6802310cf09b566e3e81df18b9a3fa1206e79
Daniel Sousa <mail@danielsousa.io>
2020-10-12 17:07:22 +0100

    Update components

--- src/features/BlankScreen4955/screens/blank.js
+++ src/features/BlankScreen4955/screens/blank.js
@@ -1,24 +1,34 @@
-import React, { Component } from "react"
-import { View, Image, ImageBackground } from "react-native"
-import { Text, Layout, Button } from "react-native-ui-kitten"
+import React from "react"
+import { View } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input
+} from "react-native-ui-kitten"
 
 import Slider from "@react-native-community/slider"
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 import { connect } from "react-redux"
-import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
-import { styles } from "./styles"
-import * as calendarActions from "../../redux/actions"
-import ErrorBox from "../../../../components/ErrorBox"
-
 export class __Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
   render = () => (
-    <CbFlex title="Button12345" color="#FF8040" style={styles.CbFlex_undefined}>
-      <CbFlex style={styles.CbFlex_undefined} />
-    </CbFlex>
+    <View style={styles.View_1}>
+      <Button
+        title="something"
+        style={styles.Button_4}
+        onPress={() => alert("Pressed!")}
+      >
+        Buttonok
+      </Button>
+    </View>
   )
 }
 
