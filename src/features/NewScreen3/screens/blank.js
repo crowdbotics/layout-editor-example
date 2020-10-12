43e1ead7ed47fe9884d5070240a805b2b4bab8e4
Crowdbotics <git@crowdbotics.com>
2020-10-12 16:43:10 +0000

    Update components

--- src/features/NewScreen3/screens/blank.js
+++ src/features/NewScreen3/screens/blank.js
@@ -16,6 +16,12 @@ import Slider from "@react-native-community/slider"
 import { CheckBox } from "react-native-elements"
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 import { connect } from "react-redux"
+import { Text, Layout, Button } from "react-native-ui-kitten"
+import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
+import { styles } from "./styles"
+import * as calendarActions from "../../redux/actions"
+import ErrorBox from "../../../../components/ErrorBox"
+
 export class New extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
