d60a32c3e00f9b0783a461a3e18c1c141e9b0fed
Crowdbotics <git@crowdbotics.com>
2020-12-03 19:07:04 +0000

    Update components

--- src/features/BlankScreen4955/screens/blank.js
+++ src/features/BlankScreen4955/screens/blank.js
@@ -26,26 +26,13 @@ export class _Blank extends React.Component {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   render = () => (
-    <CbFlex title="Button12345" color="#FF8000" style={styles.CbFlex_undefined}>
+    <CbFlex title="Button12345" color="#FF8040" style={styles.CbFlex_undefined}>
       <CbFlex style={styles.CbFlex_undefined} />
     </CbFlex>
   )
 }
 
-const styles = StyleSheet.create({
-  View_1: {
-    overflow: "visible",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderStyle: "solid",
-    backgroundSize: "auto"
-  },
-
-  Button_4: { overflow: "visible", borderStyle: "solid" }
-})
+const styles = StyleSheet.create({})
 
 function mapStateToProps(state) {
   return { state: state.apiReducer }
