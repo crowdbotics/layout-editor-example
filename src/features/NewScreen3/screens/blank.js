5419d1aaadda84b9e488db74e5608e739cf2ba07
Crowdbotics <git@crowdbotics.com>
2020-12-08 17:12:23 +0000

    Update components

--- src/features/NewScreen3/screens/blank.js
+++ src/features/NewScreen3/screens/blank.js
@@ -27,16 +27,68 @@ export class New extends React.Component {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   render = () => (
-    <CbFlex title="Button12345" color="#FF8040" style={styles.CbFlex_undefined}>
-      <CbFlex style={styles.CbFlex_undefined} />
-    </CbFlex>
+    <View title="Button12345" color="#FF8000" style={styles.View_undefined}>
+      <View style={styles.View_undefined} />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({})
 
 function mapStateToProps(state) {
-  return {}
+  return {
+    petsundefinedundefined: state.apiReducer.pets
+      .filter(
+        petsundefinedundefined => petsundefinedundefined.name == "Lorem Ipsum"
+      )
+      .filter(
+        petsundefinedundefined => petsundefinedundefined.name != "Lorem Ipsum"
+      )
+      .filter(petsundefinedundefined => petsundefinedundefined.age > 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age <= 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age >= 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age < 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age < 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age >= 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age <= 25)
+      .filter(petsundefinedundefined => petsundefinedundefined.age > 25)
+      .filter(petsundefinedundefined => !petsundefinedundefined.name)
+      .filter(petsundefinedundefined => !petsundefinedundefined.age)
+      .filter(petsundefinedundefined => {
+        if (petsundefinedundefined.name)
+          return petsundefinedundefined.name.toString().includes("lorem")
+      })
+      .filter(petsundefinedundefined => {
+        if (petsundefinedundefined.age)
+          return petsundefinedundefined.age.toString().includes(25)
+      })
+      .filter(petsundefinedundefined => petsundefinedundefined.name)
+      .filter(petsundefinedundefined => petsundefinedundefined.age)
+      .filter(petsundefinedundefined => {
+        if (petsundefinedundefined.name)
+          return !petsundefinedundefined.name.toString().includes("lorem")
+      })
+      .filter(petsundefinedundefined => {
+        if (petsundefinedundefined.age)
+          return !petsundefinedundefined.age.toString().includes(25)
+      })
+      .sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
+      .sort((a, b) => new Number(a.value) - new Number(b.value))
+      .sort((a, b) => {
+        var valueA = a.name.toUpperCase()
+        var valueB = b.name.toUpperCase()
+        if (valueA < valueB) {
+          return -1
+        } else if (valueA > valueB) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+      .reverse()
+      .sort((a, b) => new Boolean(a.public) - new Boolean(b.public))
+      .reverse()
+  }
 }
 
 const actionCreators = {}
