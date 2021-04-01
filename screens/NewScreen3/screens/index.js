85c8b0fb980a2d83c029aaf544d6aeebaa7f5724
Daniel Sousa <mail@danielsousa.io>
2021-04-01 14:40:06 +0100

    Update components

---
+++ screens/NewScreen3/screens/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+import { widthPercentageToDP as wp } from "react-native-responsive-screen"
+export class New extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  render = () => (
+    <View title="Button12345" color="#FF8000" style={styles.View_undefined}>
+      <View style={styles.View_undefined} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(New)
