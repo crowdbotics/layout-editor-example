ff284c01cf56ed925c2ea8dbd7866caa01437548
Aline Tavares <alaisgomes@gmail.com>
2021-04-05 15:10:00 -0400

    Update store.

--- connectors/swaggerPetstore/pets.slice.js
+++ connectors/swaggerPetstore/pets.slice.js
@@ -1,1 +1,128 @@
-undefined
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./services"
+export const findPetsByStatus = createAsyncThunk(
+  "pets/findPetsByStatus",
+  async payload => await apiService.findPetsByStatus(payload)
+)
+export const findPetsByTags = createAsyncThunk(
+  "pets/findPetsByTags",
+  async payload => await apiService.findPetsByTags(payload)
+)
+export const getPetById = createAsyncThunk(
+  "pets/getPetById",
+  async payload => await apiService.getPetById(payload)
+)
+export const updatePetWithForm = createAsyncThunk(
+  "pets/updatePetWithForm",
+  async payload => await apiService.updatePetWithForm(payload)
+)
+export const deletePet = createAsyncThunk(
+  "pets/deletePet",
+  async payload => await apiService.deletePet(payload)
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const petsSlice = createSlice({
+  name: "pets",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [findPetsByStatus.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [findPetsByStatus.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [...state.entities, ...action.payload]
+        state.api.loading = "idle"
+      }
+    },
+    [findPetsByStatus.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [findPetsByTags.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [findPetsByTags.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [...state.entities, ...action.payload]
+        state.api.loading = "idle"
+      }
+    },
+    [findPetsByTags.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [getPetById.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [getPetById.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
+      }
+    },
+    [getPetById.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [updatePetWithForm.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [updatePetWithForm.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
+      }
+    },
+    [updatePetWithForm.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [deletePet.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [deletePet.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.filter(
+          record => record.id !== action.starter.id
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [deletePet.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default {
+  findPetsByStatus,
+  findPetsByTags,
+  getPetById,
+  updatePetWithForm,
+  deletePet,
+  slice: petsSlice
+}
--- connectors/testePermissionsAPI/homePages.slice.js
+++ connectors/testePermissionsAPI/homePages.slice.js
@@ -1,1 +1,108 @@
-undefined
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./services"
+export const api_v1_homepage_list = createAsyncThunk(
+  "homePages/api_v1_homepage_list",
+  async payload => await apiService.api_v1_homepage_list(payload)
+)
+export const api_v1_homepage_read = createAsyncThunk(
+  "homePages/api_v1_homepage_read",
+  async payload => await apiService.api_v1_homepage_read(payload)
+)
+export const api_v1_homepage_update = createAsyncThunk(
+  "homePages/api_v1_homepage_update",
+  async payload => await apiService.api_v1_homepage_update(payload)
+)
+export const api_v1_homepage_partial_update = createAsyncThunk(
+  "homePages/api_v1_homepage_partial_update",
+  async payload => await apiService.api_v1_homepage_partial_update(payload)
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const homePagesSlice = createSlice({
+  name: "homePages",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [api_v1_homepage_list.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_homepage_list.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [...state.entities, ...action.payload]
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_list.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_read.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_homepage_read.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_read.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_update.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_homepage_update.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record =>
+          record.id === action.payload.id ? action.payload : record
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_update.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_partial_update.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_homepage_partial_update.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record =>
+          record.id === action.payload.id ? action.payload : record
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_homepage_partial_update.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default {
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  slice: homePagesSlice
+}
