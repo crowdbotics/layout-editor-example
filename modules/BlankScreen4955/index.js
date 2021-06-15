5a1dafa22bae029b99d87756bc8f7e3597102994
Crowdbotics <git@crowdbotics.com>
2021-06-15 14:27:01 +0000

    Update components

---
+++ modules/BlankScreen4955/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@modules"
+export class _Blank extends React.Component {
+  state = {}
+  render = () => (
+    <View title="Button12345" color="#FF8000" style={styles.View_undefined}>
+      <View style={styles.View_undefined} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default {
+  title: "BlankScreen4955",
+  navigator: connect(mapStateToProps, mapDispatchToProps)(_Blank)
+}
