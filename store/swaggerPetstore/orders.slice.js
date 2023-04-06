6f022d4c1df23c24a3dc3b5e363fc9059778182b
Crowdbotics <engineering@crowdbotics.com>
2023-04-06 15:56:53 +0000

    Update store.

---
+++ store/swaggerPetstore/orders.slice.js
@@ -0,0 +1,88 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const placeOrder = createAsyncThunk(
+  "orders/placeOrder",
+  async payload => {
+    const response = await apiService.placeOrder(payload)
+    return response.data
+  }
+)
+export const getOrderById = createAsyncThunk(
+  "orders/getOrderById",
+  async payload => {
+    const response = await apiService.getOrderById(payload)
+    return response.data
+  }
+)
+export const deleteOrder = createAsyncThunk(
+  "orders/deleteOrder",
+  async payload => {
+    const response = await apiService.deleteOrder(payload)
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const ordersSlice = createSlice({
+  name: "orders",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [placeOrder.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [placeOrder.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
+      }
+    },
+    [placeOrder.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [getOrderById.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [getOrderById.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
+      }
+    },
+    [getOrderById.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [deleteOrder.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [deleteOrder.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.filter(
+          record => record.id !== action.meta.arg?.id
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [deleteOrder.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default { placeOrder, getOrderById, deleteOrder, slice: ordersSlice }
