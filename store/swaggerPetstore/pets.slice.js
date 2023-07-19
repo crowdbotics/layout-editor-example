d9a8b00c035a997bcb8da15a47c26bc983693b9e
Aline Tavares <alaisgomes@gmail.com>
2023-07-19 13:55:12 -0400

    Update store.

--- store/swaggerPetstore/pets.slice.js
+++ store/swaggerPetstore/pets.slice.js
@@ -34,97 +34,98 @@ const petsSlice = createSlice({
   name: "pets",
   initialState,
   reducers: {},
-  extraReducers: {
-    [findPetsByStatus.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [findPetsByStatus.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
-      }
-    },
-    [findPetsByStatus.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [findPetsByTags.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [findPetsByTags.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
-      }
-    },
-    [findPetsByTags.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [getPetById.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [getPetById.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = [
-          ...state.entities.filter(record => record.id !== action.payload.id),
-          action.payload
-        ]
-        state.api.loading = "idle"
-      }
-    },
-    [getPetById.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [updatePetWithForm.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [updatePetWithForm.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
-      }
-    },
-    [updatePetWithForm.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [deletePet.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [deletePet.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.filter(
-          record => record.id !== action.meta.arg?.id
-        )
-        state.api.loading = "idle"
-      }
-    },
-    [deletePet.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    }
+  extraReducers: builder => {
+    builder
+      .addCase(findPetsByStatus.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(findPetsByStatus.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = action.payload
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(findPetsByStatus.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(findPetsByTags.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(findPetsByTags.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = action.payload
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(findPetsByTags.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(getPetById.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(getPetById.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = [
+            ...state.entities.filter(record => record.id !== action.payload.id),
+            action.payload
+          ]
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(getPetById.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(updatePetWithForm.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(updatePetWithForm.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities.push(action.payload)
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(updatePetWithForm.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(deletePet.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(deletePet.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.filter(
+            record => record.id !== action.meta.arg?.id
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(deletePet.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
   }
 })
 export default {
